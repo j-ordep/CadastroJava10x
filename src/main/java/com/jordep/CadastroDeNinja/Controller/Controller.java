70d67f0a0384b9a2991ef5851c2cb9fb465316f2
jordep <joaoppinheiro2005@gmail.com>
2025-03-14 10:30:47 -0300

    Controller criado

---
+++ src/main/java/com/jordep/CadastroDeNinja/Controller/Controller.java
@@ -0,0 +1,23 @@
+package com.jordep.CadastroDeNinja.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/helloWorld")
+    public String getHelloWorld() {
+        return "minha primeira rota GET";
+    }
+
+    @PostMapping("/helloWorld")
+    public String postHelloWorld(String falaAlgumaCoisaMacaco) {
+        return "minha primeira rota POST";
+    }
+
+
+}
