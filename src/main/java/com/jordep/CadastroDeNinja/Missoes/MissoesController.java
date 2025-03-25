d4d88998e68aed371f64da6fdc819492cd159c0c
jordep <joaoppinheiro2005@gmail.com>
2025-03-24 22:54:32 -0300

    feat: controller created with mock

--- src/main/java/com/jordep/CadastroDeNinja/Missoes/MissoesController.java
+++ src/main/java/com/jordep/CadastroDeNinja/Missoes/MissoesController.java
@@ -1,4 +1,29 @@
 package com.jordep.CadastroDeNinja.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping
+@RestController
 public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissaos() {
+        return "missao listada";
+    }
+
+    @PostMapping ("/criar")
+    public String criarMissoes() {
+        return "missao criada";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "missao alterada";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "missao deletada";
+    }
+
 }
