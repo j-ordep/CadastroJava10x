ee4fa9bc9cffb20e3e3a8c3abf9f5c7a12b8d48c
jordep <joaoppinheiro2005@gmail.com>
2025-03-24 22:11:55 -0300

    feat: update controller with mock data

--- src/main/java/com/jordep/CadastroDeNinja/Ninjas/Controller/NinjaController.java
+++ src/main/java/com/jordep/CadastroDeNinja/Ninjas/Controller/NinjaController.java
@@ -1,4 +1,41 @@
 package com.jordep.CadastroDeNinja.Ninjas.Controller;
 
+import com.jordep.CadastroDeNinja.Ninjas.Model.NinjaModel;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping
+@RestController
 public class NinjaController {
+
+    // Adiconar ninja - (CREATE)
+    @PostMapping("/criarNinja")
+    public String criarNinja() {
+        return "Ninja Criado";
+    }
+
+    // Mostrar todos os Ninjas - (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinja() {
+        return "todos os ninjas";
+    }
+
+
+    // Procurar Ninja por ID - (CREATE)
+    @GetMapping("/todosId")
+    public String mostrarNinja() {
+        return "Mostrando ninja por id";
+    }
+
+    // Alterar dados do Ninja - (UPDATE)
+    @PutMapping("/alterar")
+    public String alterarNinja(@PathVariable Long ninjaId){
+        return "atualizando ninja";
+    }
+
+    // Deletar Ninja - (DELETE)
+    @DeleteMapping("/deletar")
+    public String deletarNinja() {
+        return "deletado";
+    }
+
 }
